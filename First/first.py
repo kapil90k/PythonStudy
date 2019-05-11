3df50167d3f582d631ae69f29a3e0e901df535bd
kapil90k <kapil.mnnit90@gmail.com>
2019-05-11 13:10:31 +0530

    commit message

---
+++ First/first.py
@@ -0,0 +1,6 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Kapil').master('local').getOrCreate()
+df = spark.createDataFrame([(1,'One'),(2,'Two')],['col1','col2'])
+df.show()
+spark.stop()
